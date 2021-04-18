04fd2eb4cab81841d12fd7d8c0f6bd00714d65a1
Rahul Tiwari <rahul.s.tiwari9@gmail.com>
2021-04-18 13:57:45 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ function App() {
               <p>{note.description}</p>
               <button onClick={() => deleteNote(note)}>Delete note</button>
               {
-                note.image && <img src={note.image} style={{ width: 400 }} />
+                note.image && <img alt="" src={note.image} style={{ width: 400 }} />
               }
             </div>
           ))
